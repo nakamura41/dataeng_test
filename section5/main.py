bdc5699e9133a6888709fbe265dbc28b8f0659e8
David Leonardi <david.leonardi@axa.com.sg>
2021-09-04 17:30:46 +0800

    Section5: complete machine learning model.

--- section5/main.py
+++ section5/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OrdinalEncoder
 
 if __name__ == "__main__":
@@ -17,18 +18,44 @@ if __name__ == "__main__":
     attributes = ["maint", "doors", "lug_boot", "safety", "class"]
     target = ["buying"]
 
-    categories = [
-        ["low", "med", "high", "vhigh"],  # buying category
-        ["low", "med", "high", "vhigh"],  # maintenance category
-        ["2", "3", "4", "5more"],  # doors category
-        ["small", "med", "big"],  # lug_boot category
-        ["low", "med", "high"],  # safety category
-        ["unacc", "acc", "good", "vgood"],  # class category
+    attributes_categories = [
+        ["low", "med", "high", "vhigh"],  # maintenance
+        ["2", "3", "4", "5more"],  # number of doors
+        ["small", "med", "big"],  # lug boot size
+        ["low", "med", "high"],  # safety
+        ["unacc", "acc", "good", "vgood"],  # class value
     ]
-    ordinal_encoder = OrdinalEncoder(categories=categories)
-    print(ordinal_encoder)
 
-    ordinal_encoder.fit(df[target + attributes])
+    target_category = [
+        ["low", "med", "high", "vhigh"]  # buying
+    ]
 
-    data = ordinal_encoder.transform(df[target + attributes])
-    print(data)
+    attributes_encoder = OrdinalEncoder(categories=attributes_categories)
+    attributes_encoder.fit(df[attributes])
+    x = attributes_encoder.transform(df[attributes])
+
+    target_encoder = OrdinalEncoder(categories=target_category)
+    target_encoder.fit(df[target])
+    y = target_encoder.transform(df[target])
+
+    regression = LinearRegression()
+    regression.fit(x, y)
+
+    prediction_attributes = [[
+        "high",  # maintenance
+        "4",  # number of doors
+        "big",  # lug boot size
+        "high",  # safety
+        "good"  # class value
+    ]]
+
+    encoded_attributes = attributes_encoder.transform(prediction_attributes)
+    print(encoded_attributes)
+    
+    print(attributes_encoder.inverse_transform(encoded_attributes))
+    
+    prediction = regression.predict(encoded_attributes)
+    print(prediction)
+
+    decoded_prediction = target_encoder.inverse_transform(prediction)
+    print(decoded_prediction)
