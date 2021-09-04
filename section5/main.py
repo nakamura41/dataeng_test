1a526a1789eedd38a784b2477018a25d4ddcbc1d
David Leonardi <david.leonardi@axa.com.sg>
2021-09-04 17:10:34 +0800

    Section5: remove dirty data (unacc      g).

--- section5/main.py
+++ section5/main.py
@@ -21,8 +21,14 @@ if __name__ == "__main__":
         ["low", "med", "high", "vhigh"],  # buying category
         ["low", "med", "high", "vhigh"],  # maintenance category
         ["2", "3", "4", "5more"],  # doors category
-        ["small", "med", "big"],  # safety category
+        ["small", "med", "big"],  # lug_boot category
+        ["low", "med", "high"],  # safety category
         ["unacc", "acc", "good", "vgood"],  # class category
     ]
     ordinal_encoder = OrdinalEncoder(categories=categories)
     print(ordinal_encoder)
+
+    ordinal_encoder.fit(df[target + attributes])
+
+    data = ordinal_encoder.transform(df[target + attributes])
+    print(data)
